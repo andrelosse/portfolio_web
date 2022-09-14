b88822d02707b9021d77c1a4d04272416474c996
andrelosse <andreloze@gmail.com>
2022-09-13 21:25:32 -0300

    Small editions for improved code writting

--- src/constcontent/index.js
+++ src/constcontent/index.js
@@ -21,30 +21,35 @@ export const projects = [
         icon: "minkvpn",
         title: "MinkVPN",
         content: "VPN client with user interface, made with C# and XAML using MVVM architecture.",
+        url: 'https://github.com/andrelosse/MinkVPN',
     },
     {
         id: "portproj",
         icon: "portproj",
         title: "Portfolio Project",
         content: "Personal portfolio project made with React and Tailwind CSS.",
+        url: 'https://github.com/andrelosse/portfolio_web',
     },
     {
         id: "fibonacci",
         icon: "fibo",
         title: "Fibonacci Sequence",
         content: "Console application to generate a Fibonacci sequence with a user defined lenght.",
+        url: 'https://github.com/andrelosse/fibonacci_sequence',
     },
     {
         id: "location",
         icon: "location",
         title: "Location Searcher",
         content: "Console application that searches for a given address and writes the information to a JSON file.",
+        url: 'https://github.com/andrelosse/location_searcher',
     },
     {
         id: "soon",
         icon: "soon",
         title: "Coming Soon",
         content: "This project will be revealed soon...",
+        url: '',
     }
 ];
 
